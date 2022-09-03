edf167dcfd8c79b81869d7e5828d297821eb84e1
KihnGodas <106853150+KihnGodas@users.noreply.github.com>
2022-09-03 15:12:02 +0700

    Update tab.js

--- finale/js/tab.js
+++ finale/js/tab.js
@@ -1,16 +1,27 @@
 
-var btn = document.querySelector('.btn');
+var des = document.getElementById('des')
+var spec = document.getElementById('spec')
 var description = document.querySelector('.description');
-btn.addEventListener('click',function(){
+var specification = document.querySelector('.specification')
+des.addEventListener('click',function(){
    if(description.style.display ==='none'){
     description.style.display ='block'
     description.style.animation = 'showDiv 1s'
    }
-   else{
-    description.style.display='none'
-    description.style.animation = 'hideDiv 1s'
+   else{description.style.animation = 'hideDiv 1s';
+    setTimeout(() => {
+        description.style.display = 'none'
+    }, 1000);
    }
 })
-btn.addEventListener('click',function(){
-    specification.style.animation = 'showDiv 1s'
+spec.addEventListener('click',function(){
+    if(specification.style.display ==='none'){
+        specification.style.display ='block'
+        specification.style.animation = 'showDiv 1s'
+    }else{
+        specification.style.animation = 'hideDiv 1s';
+    setTimeout(() => {
+        specification.style.display = 'none'
+    }, 1000);
+    }
 })
